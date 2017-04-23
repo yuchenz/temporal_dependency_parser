fbca7de72a7739c53dfb79818e08abec519d76b0
Yuchen Zhang <yuchenz@brandeis.edu>
2017-04-23 00:58:12 -0400

    add call ffNN_classifier training code

--- train.py
+++ train.py
@@ -1,13 +1,18 @@
 import sys
 import codecs
 from data_preparation import make_training_data
+from ffNN_classifier import FFNN_Classifier
 
 
 if __name__ == '__main__':
     train_file = sys.argv[1]
 
     training_data, vocab = make_training_data(train_file)
+
     for doc in training_data:
         for state, action in doc:
             print(state)
             print(action, end='\n\n')
+
+    classifier = FFNN_Classifier(8, 8, 4, vocab)
+    classifier.train(training_data, 'ffnn_model', num_iter=10)
